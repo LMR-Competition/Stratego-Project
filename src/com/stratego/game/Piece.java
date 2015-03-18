6ffc04bba44ee6597f349c46f05f940c6de29ed6
Derionicle <Derionicle@users.noreply.github.com>
2015-03-18 11:47:43 -0400

    Update Piece.java

--- src/com/stratego/game/Piece.java
+++ src/com/stratego/game/Piece.java
@@ -3,14 +3,17 @@ package com.stratego.game;
 /** Rank 1-9 as expected, rank10=spy, Rank11=bomb, rank12=flag|| side 0=red | side 1=blue*/
 public class Piece {
 	
-	public static int[] soldierLocation = new int[2];
-	
 	public static int soldierSide;
 	
 	public static int soldierRank;
 	
-	public Piece(int rank, int side){
-		
+	public static int x;
+	
+	public static int y;
+	
+	public Piece(int newx, int newy, int rank, int side){
+		x=newx;
+		y=newy;
 		soldierRank = rank;
 		soldierSide = side;
 	}
