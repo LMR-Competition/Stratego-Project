bd09706afdf73aaffbd11b93d3d42342683226b1
Derionicle <Derionicle@users.noreply.github.com>
2015-03-18 11:44:23 -0400

    Update Piece.java

--- src/com/stratego/game/Piece.java
+++ src/com/stratego/game/Piece.java
@@ -1,3 +1,4 @@
+//As of right now this class is used for creation and then all further functions are done by referenceing the piece stored on gameBoard
 package com.stratego.game;
 /** Rank 1-9 as expected, rank10=spy, Rank11=bomb, rank12=flag|| side 0=red | side 1=blue*/
 public class Piece {
