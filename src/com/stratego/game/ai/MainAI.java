7468f65218772504dfb2a5aa99b9af2590e20f32
Derionicle <Derionicle@users.noreply.github.com>
2015-03-19 09:16:15 -0400

    Update MainAI.java

--- src/com/stratego/game/ai/MainAI.java
+++ src/com/stratego/game/ai/MainAI.java
@@ -1,12 +1,26 @@
 package com.stratego.game.ai;
 
+import com.stratego.game.*;
+
 public class MainAI{
+  int aiSide = 0;
   public static void aiTurn(){
     //weighting?
   }
   public static Tile aiFindNearest(Tile home){
-    closestTile =
-    //
+    Tile closestTile = gameBoard[0][0];
+    
+    for (int x = 0; x<10; x++){
+      for (int y = 0; y<10;y++){
+        if (gameBoard[x][y].side != aiSide){
+          
+        }
+      }
+    }
+    //Calc closest piece
     return closestTile;
+    public static void setBoard(){
+      
+    }
   }
 }
