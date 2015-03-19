9c63488c3ad0d6bfadf927c0f3b2c904bec0ab49
Derionicle <Derionicle@users.noreply.github.com>
2015-03-19 09:01:48 -0400

    Update MainAI.java

--- src/com/stratego/game/ai/MainAI.java
+++ src/com/stratego/game/ai/MainAI.java
@@ -1,1 +1,10 @@
-
+public class MainAI{
+  public static void aiTurn(){
+    //weighting?
+  }
+  public static Tile aiFindNearest(Tile home){
+    closestTile =
+    //
+    return closestTile;
+  }
+}
